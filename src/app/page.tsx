908df43d493c68f095d7deeefa3d5adfa02c1245
Tim Goalen <timgoalen@hotmail.com>
2024-02-28 15:34:30 +0000

    build: initialise create-next-app

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 contentEditable="true" className="text-blue p-2">
+        title
+      </h1>
+      <p contentEditable="true">paragraph</p>
+      <p contentEditable="true">paragraph</p>
+      <p contentEditable="true">paragraph</p>
+    </main>
+  );
+}
