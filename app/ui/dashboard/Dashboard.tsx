ed2685dad07f3b3c9a3f331d3a5ddcad95f97119
Tim Goalen <timgoalen@hotmail.com>
2024-03-07 10:40:19 +0000

    feat: reveal dashboard on hover

--- app/ui/dashboard/Dashboard.tsx
+++ app/ui/dashboard/Dashboard.tsx
@@ -1,12 +1,46 @@
-import { openSans } from "@/app/ui/fonts";
+"use client";
+import { useState } from "react";
 
+import { openSans } from "@/app/ui/fonts";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faAnglesDown } from "@fortawesome/free-solid-svg-icons";
+import { motion } from "framer-motion";
+
+import styles from "@/app/styles/Dashboard.module.css";
 import SideNav from "./SideNav";
 
 export default function Dashboard() {
+  const [isDashboardOpen, setIsDashboardOpen] = useState(false);
+
   return (
-    <section className="dashboard-container">
-      <div className={`${openSans.className} antialiased site-title`}>tasker</div>
-      <SideNav />
-    </section>
+    <>
+      {isDashboardOpen ? (
+        <div onMouseLeave={() => setIsDashboardOpen(false)}>
+          <button className={styles.icon}>
+            <FontAwesomeIcon icon={faAnglesDown} />
+          </button>
+
+          <motion.section
+            className="dashboard-container"
+            transition={{ duration: 0.2 }}
+            initial={{ x: -20, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+          >
+            <div className={`${openSans.className} antialiased site-title`}>
+              tasker
+            </div>
+
+            <SideNav />
+          </motion.section>
+        </div>
+      ) : (
+        <button
+          className={styles.icon}
+          onMouseEnter={() => setIsDashboardOpen(true)}
+        >
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+      )}
+    </>
   );
 }
