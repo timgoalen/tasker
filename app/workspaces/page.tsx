3ae1aeee1ff1de2813fafe7904aeb34499137892
Tim Goalen <timgoalen@hotmail.com>
2024-03-19 09:58:25 +0000

    style: add media queries

--- app/workspaces/page.tsx
+++ app/workspaces/page.tsx
@@ -2,13 +2,14 @@ import Header from "../ui/header/Header";
 import WorkspacesListHead from "../ui/workspaces/WorkspacesListHead";
 import WorkspaceListItem from "../ui/workspaces/WorkspaceListItem";
 import WorkspaceListSpacer from "../ui/workspaces/WorkspaceListSpacer";
+import styles from "@/app/styles/WorkspacesPage.module.css";
 
 export default function Page() {
   return (
     <>
       <Header title="Workspaces" />
-      <section className="page-container">
-        <div className="page-content">
+      <section className={styles.container}>
+        <div className={styles.content}>
           <WorkspacesListHead />
 
           <WorkspaceListItem />
